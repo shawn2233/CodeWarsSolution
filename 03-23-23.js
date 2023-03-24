72484181628b42d796cee5957b50bf6bf827a10d
Shawn Lawrence <shawnlawrence@Shawns-MacBook-Pro.local>
2023-03-23 20:31:29 -0400

    3-23-23

---
+++ 03-23-23.js
@@ -0,0 +1,8 @@
+// Convert a Number to a String!
+
+// We need a function that can transform a number (integer) into a string.
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString()
+  }
