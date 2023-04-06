018d9eebd8c1edbf32b6417a1fa0e0e9ec5945c6
Shawn Lawrence <lawshawn223@gmail.com>
2023-04-05 22:52:03 -0400

    4-5-23

---
+++ april2023/04-05-23.js
@@ -0,0 +1,6 @@
+// Multiple of index
+
+// Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+
+// Some cases:
+// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
