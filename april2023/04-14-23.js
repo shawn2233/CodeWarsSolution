223f5740eb1ed39415410b082642b284a5a72b9b
Shawn Lawrence <lawshawn223@gmail.com>
2023-04-14 19:56:07 -0400

    4-14-23

---
+++ april2023/04-14-23.js
@@ -0,0 +1,16 @@
+// Well of Ideas - Easy Version
+
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+function well(x){
+    let newArray = x.filter(item => item === "good")
+    if(newArray.length === 2 || newArray.length === 1){
+      return "Publish!"
+    }else if(newArray.length > 2){
+      return "I smell a series!"
+    }else{
+      return "Fail!"
+    }
+  }
