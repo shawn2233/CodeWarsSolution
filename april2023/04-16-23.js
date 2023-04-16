4cf92d4ac934d1f6d9cb93843b3024a775ab3537
Shawn Lawrence <lawshawn223@gmail.com>
2023-04-16 09:56:26 -0400

    4-16-23

---
+++ april2023/04-16-23.js
@@ -0,0 +1,12 @@
+// Multiply
+
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//     a * b
+//   }
+  
+function multiply(a, b){
+    return a * b
+  }
+  
