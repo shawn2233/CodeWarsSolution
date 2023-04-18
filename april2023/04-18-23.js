d9ee4e0a61f927e5e52a1906bc1fdfdc7d42e736
Shawn Lawrence <lawshawn223@gmail.com>
2023-04-18 00:54:11 -0400

    4-18-23

---
+++ april2023/04-18-23.js
@@ -0,0 +1,16 @@
+// Convert boolean values to strings 'Yes' or 'No'.
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+function boolToWord( bool ){
+  if(bool === true){
+    return "Yes"
+  }else{
+    return "No"
+  }
+}
+
+//or
+
+const boolToWord = bool => bool === true ? "Yes" : "No"
